7db2dac1fc8853bfe8e6acd643eb23a3b08af34f
Michele Giacomini <michele.giacomini91@gmail.com>
2022-10-07 16:38:39 +0200

    Change back email to personal one

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Michele Giacomini"
-      user-mail-address "michele@quasarud.it")
+      user-mail-address "michele.giacomini91@gmail.com")
 
 ;; Better? defaults from https://tecosaur.github.io/emacs-config/config.html#rudimentary-configuration
 (setq-default
