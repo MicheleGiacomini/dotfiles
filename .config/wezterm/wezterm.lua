493bf1ececf0b152862c8c0568309b1c05692a88
Michele Giacomini <michele@quasarud.it>
2024-11-04 14:19:33 +0100

    Add wezterm config from work pc

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,76 @@
 local wezterm = require("wezterm")
-local config = {}
+local act = wezterm.action
+local config = wezterm.config_builder()
 
+config.term = "wezterm"
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 9.3
 config.color_scheme = "rose-pine-moon"
+config.default_prog = { "nu" }
+config.automatically_reload_config = true
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+-- config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.enable_kitty_keyboard = true
+
+-- TABLINE
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+-- tabline.apply_to_config(config)
+tabline.setup({
+	options = {
+		theme = "rose-pine-moon",
+	},
+	sections = {
+		tabline_x = {},
+	},
+	extensions = { "smart_workspace_switcher" },
+})
+
+-- SMART WORKSPACE SWITCHER
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+workspace_switcher.apply_to_config(config)
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+	{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+	{ key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+	{ key = "4", mods = "LEADER", action = act.ActivateTab(3) },
+	{ key = "5", mods = "LEADER", action = act.ActivateTab(4) },
+	{ key = "6", mods = "LEADER", action = act.ActivateTab(5) },
+	{ key = "7", mods = "LEADER", action = act.ActivateTab(6) },
+	{ key = "8", mods = "LEADER", action = act.ActivateTab(7) },
+	{ key = "9", mods = "LEADER", action = act.ActivateTab(-1) },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "o", mods = "LEADER", action = workspace_switcher.switch_workspace() },
+	{
+		key = "R",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			-- initial_value = wezterm.mux.get_active_workspace(),
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Rename workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line), pane)
+				end
+			end),
+		}),
+	},
+}
 
 return config
